2f52f5fe3dfb57a23dbf639de5abb89e50b9952a
LucaBloise <lbloise@itba.edu.ar>
2024-11-02 05:18:54 -0300

    Query2Client Finished

--- client/src/main/java/ar/edu/itba/pod/client/query/Query2Client.java
+++ client/src/main/java/ar/edu/itba/pod/client/query/Query2Client.java
@@ -1,19 +1,26 @@
 package ar.edu.itba.pod.client.query;
 
 import ar.edu.itba.pod.api.model.Ticket;
+import ar.edu.itba.pod.api.model.dto.AgencyYearMonthYTD;
+import ar.edu.itba.pod.api.query2.YTDByAgencyCombinerFactory;
+import ar.edu.itba.pod.api.query2.YTDByAgencyMapper;
+import ar.edu.itba.pod.api.query2.YTDByAgencyReducerFactory;
 import ar.edu.itba.pod.client.util.QueryPropertiesFactory;
+import com.hazelcast.core.ICompletableFuture;
 import com.hazelcast.core.IMap;
 import com.hazelcast.mapreduce.KeyValueSource;
 import com.hazelcast.mapreduce.JobTracker;
 import com.hazelcast.mapreduce.Job;
+import query2.YTDByAgencyCollator;
 
+import java.util.SortedSet;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.atomic.AtomicLong;
 
 @SuppressWarnings("deprecation")
 public class Query2Client extends QueryClient<Long, Ticket> {
     private static final String JOB_TRACKER_NAME = GROUP_NAME + "-agency-ytd";
-    private static final String[] OUT_CSV_HEADERS = {};
+    private static final String[] OUT_CSV_HEADERS = {"Agency", "Year", "Month", "YTD"};
     private static final String OUT_CSV_FILENAME = "/query2.csv";
     private static final String OUT_TIME_FILENAME = "/time2.txt";
 
@@ -38,6 +45,14 @@ public class Query2Client extends QueryClient<Long, Ticket> {
 
         JobTracker jobTracker = hazelcastInstance.getJobTracker(JOB_TRACKER_NAME);
         Job<Long, Ticket> job = jobTracker.newJob(keyValueSource);
+        ICompletableFuture<SortedSet<AgencyYearMonthYTD>> future = job
+                .mapper(new YTDByAgencyMapper())
+                .combiner(new YTDByAgencyCombinerFactory())
+                .reducer(new YTDByAgencyReducerFactory())
+                .submit(new YTDByAgencyCollator());
+
+        SortedSet<AgencyYearMonthYTD> set = future.get();
+        printResults(OUT_CSV_HEADERS, OUT_CSV_FILENAME, set);
     }
 
     public static void main(String[] args) {

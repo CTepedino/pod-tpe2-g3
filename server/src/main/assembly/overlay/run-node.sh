3630a89d6c3d942a2179a7fba16041125edcb1ea
CTepedino <ctepedino@itba.edu.ar>
2024-11-03 14:48:02 -0300

    Cleaned scripts

--- server/src/main/assembly/overlay/run-node.sh
+++ server/src/main/assembly/overlay/run-node.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-PATH_TO_CODE_BASE=`pwd`
-
 MAIN_CLASS="ar.edu.itba.pod.server.Server"
 
 java  $JAVA_OPTS -cp 'lib/jars/*' $MAIN_CLASS $*
--- tests/query1Test.sh
+++ tests/query1Test.sh
@@ -16,4 +16,6 @@ psql -U "$DB_USER" -d "$DB_NAME" <<EOF
 \copy (SELECT description AS Infraction, t.agency AS Agency, COUNT(*) AS Tickets FROM tickets t JOIN infractions i ON t.infraction_id = i.id JOIN agencies a ON t.agency = a.agency GROUP BY i.description, t.agency HAVING COUNT(*) > 0 ORDER BY Tickets DESC, Infraction, t.agency) TO '$CSV_PATH/query1Test.csv' WITH (FORMAT csv, DELIMITER ';', HEADER);
 EOF
 
-
+if [ -f $CSV_PATH/query1.csv ]; then
+  diff $CSV_PATH/query1.csv $CSV_PATH/query1Test.csv > query1Test.diff
+fi

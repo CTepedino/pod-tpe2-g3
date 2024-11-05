805bc789025330240f1cea5896d78d759e68b566
CTepedino <ctepedino@itba.edu.ar>
2024-11-05 19:58:35 -0300

    Updated query3test.sh to also call query3.sh

--- tests/query3Test.sh
+++ tests/query3Test.sh
@@ -14,23 +14,17 @@ TO="$6"
 
 rm -f "$CSV_PATH/query3Test.csv"
 
+
+cd ../client/target/tpe2-g3-client-1.0-SNAPSHOT-bin/tpe2-g3-client-1.0-SNAPSHOT
+
+sh query3.sh -Daddresses="$ADDRESSES" -Dcity=NYC  -DinPath=../../../../tests/resources/ -DoutPath=../../../../tests/resources/ -Dn="$N" -Dfrom="$FROM" -Dto="$TO"
+
+cd ../../../../tests
+
 psql -U "$DB_USER" -d "$DB_NAME" <<EOF
 \copy (WITH repeat_offenders AS (SELECT county, plate, infraction_id FROM tickets WHERE issue_date BETWEEN '$FROM' AND '$TO' GROUP BY county, plate, infraction_id HAVING COUNT(*) >= $N), repeat_offenders_per_county AS (SELECT county, COUNT(DISTINCT plate) as total_repeat_offenders FROM repeat_offenders GROUP BY county), driver_per_county AS (SELECT county, COUNT(DISTINCT plate) AS total_drivers FROM tickets WHERE issue_date BETWEEN '$FROM' AND '$TO' GROUP BY county) SELECT d.county AS "County", CASE WHEN d.total_drivers = 0 THEN '0.00%' WHEN COALESCE(r.total_repeat_offenders, 0) = 0 THEN '0.00%' ELSE TO_CHAR((COALESCE(r.total_repeat_offenders, 0) * 100.0) / NULLIF(d.total_drivers, 0), 'FM999.00') || '%' END AS "Percentage" FROM driver_per_county d LEFT JOIN repeat_offenders_per_county r ON d.county = r.county ORDER BY (COALESCE(r.total_repeat_offenders, 0) * 100.0) / NULLIF(d.total_drivers, 0) DESC) TO '$CSV_PATH/query3Test.csv' WITH (FORMAT csv, DELIMITER ';', HEADER);
 EOF
 
-if [ -s "$CSV_PATH/query3Test.csv" ]; then
-    echo "Archivo CSV generado correctamente: $CSV_PATH/query3Test.csv"
-else
-    echo "Error: El archivo CSV no se generó o está vacío."
-    exit 1
-fi
-
-
 if [ -f "$CSV_PATH/query3.csv" ]; then
     diff "$CSV_PATH/query3.csv" "$CSV_PATH/query3Test.csv" > query3Test.diff
-    if [ -s query3Test.diff ]; then
-        echo "Diferencias encontradas entre los archivos."
-    else
-        echo "Los archivos son idénticos."
-    fi
 fi
